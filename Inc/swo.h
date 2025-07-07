c5df3479ffd6a1165102346e1d89854db811f296
AH-Bagheri <whitewolf97@yahoo.com>
2025-07-07 11:03:21 +0330

    Bit banding macros added

--- Inc/swo.h
+++ Inc/swo.h
@@ -12,8 +12,8 @@
 
 
 
-#define YES                             1
-#define NO                              0
+#define YES                            1
+#define NO                             0
 
 #define IS_INSIDE_INTERRUPT()          (__get_IPSR() != 0)
 
@@ -40,18 +40,24 @@
 #define CONCAT(...)                    POSITION(__VA_ARGS__)
 
                                         
-#define ___COUNT_(a1,  a2,  a3,  a4,  a5,  a6,  a7,  a8,  a9,  a10,\
+#define ___COUNT_(a01, a02, a03, a04, a05, a06, a07, a08, a09, a10,\
                   a11, a12, a13, a14, a15, a16, a17, a18, a19, a20,\
                   a21, a22, a23, a24, a25, a26, a27, a28, a29, a30,\
                   a31, a32, a33, a34, a35, a36, a37, a38, a39, a40,\
                   a41, a42, a43, a44, a45, a46, a47, a48, a49, a50,\
                   N, ...)              N
 
-#define COUNT(...)                     ___COUNT_(__VA_ARGS__, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41,\
+#define COUNT(...)                     ___COUNT_(__VA_ARGS__,  50, 49, 48, 47, 46, 45, 44, 43, 42, 41,\
                                                                40, 39, 38, 37, 36, 35, 34, 33, 32, 31,\
                                                                30, 29, 28, 27, 26, 25, 24, 23, 22, 21,\
                                                                20, 19, 18, 17, 16, 15, 14, 13, 12, 11,\
                                                                10,  9,  8,  7,  6,  5,  4,  3,  2, 1)
+
+
+#define __BB_GET_ACCESS(__base, __offset, __bit)\
+                                       (*(uint32_t*)(PERIPH_BB_BASE + (__base + __offset) * 0x20 + (__bit * 0x04)))
+
+#define BB_GPIO_ODR(port, pin)         __BB_GET_ACCESS(CONCAT(GPIO, CONCAT(port, _BASE)), 0x10, pin)
 
 
 
